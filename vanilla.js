9a580da8d66fa756d7cb59e25ca030d2002a57db
Sheena Vasani <sheenavasani1@gmail.com>
2017-06-26 14:51:42 -0700

    final commit

---
+++ vanilla.js
@@ -0,0 +1,18 @@
+// ====================================================================================================
+//
+// Text Analyzer
+//
+// ====================================================================================================
+
+// --------------------------------------------------
+// Initialize when the DOM is ready
+// --------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    var textarea   = document.getElementById('js-text'),
+        submitBtn  = document.getElementById('js-submit'),
+        countElem  = document.querySelector('.js-wordcount'),
+        uniqueElem = document.querySelector('.js-unique'),
+        avgElem    = document.querySelector('.js-average'),
+        report     = document.querySelector('.text-report');
+});
